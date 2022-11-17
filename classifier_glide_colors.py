12247cd4c1ac664c5655b32d9c6f3647a56f0d7f
nameless-mc <s.taniguchi.zip@gmail.com>
2022-11-17 06:25:50 +0000

    color images 用の学習を追加

---
+++ classifier_glide_colors.py
@@ -0,0 +1,61 @@
+import glob
+import json
+import os
+import torch
+import torch.utils.data
+from classifier import classifier
+from utils.dataset import ImageData, ImageDataImg, ImageDataset
+from utils.image_transform import ImageTransform
+from utils.image_transform_img import ImageTransformImg
+from utils.model import create_model
+import numpy as np
+
+
+def get_img_name_from_path(key: str):
+    return key.split("/")[-3]
+
+
+def change_dict_key(d):
+    nd = {}
+    for key in d.keys():
+        new_key = get_img_name_from_path(key)
+        nd[new_key] = d[key]
+    return nd
+
+
+def main(target_step):
+    model_epoch = 5
+    batch_size = 32
+    project = "vgg16-color-step-images"
+    model_path = f"models/{project}/{target_step}/model_{model_epoch}.pth"
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model = create_model(False, device, model_path)
+
+    transform = ImageTransformImg()
+
+    dir_path = "dataset"
+    file_list = []
+    file_path = os.path.join(f"{dir_path}/result_for_humans/img/*/steps/*/image_steps/{target_step}.png")
+    for f in glob.glob(file_path):
+        file_list.append(ImageDataImg(f, 0, transform))
+
+    dataset = ImageDataset(csv_file_list=file_list, phase='val')
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, shuffle=False)
+
+    _, _, d = classifier(
+        model, dataloader, device)
+
+    d = change_dict_key(d)
+
+    with open(os.path.join(dir_path, f"classifier_color_step_{target_step}.json"), "w") as f:
+        json.dump(d, f, indent=4, ensure_ascii=False, sort_keys=True)
+
+
+if __name__ == "__main__":
+    steps = [95, 90, 85, 80, 70, 60, 50, 40, 30, 20, 10, 0]
+    for step in steps:
+        main(step)
--- utils/image_transform_img.py
+++ utils/image_transform_img.py
@@ -3,20 +3,19 @@ import torchvision.transforms as transforms
 
 class ImageTransformImg():
 
-    def __init__(self, min=0, max=10):
+    def __init__(self):
 
         # dicに訓練用、検証用のトランスフォーマーを生成して格納
         self.data_transform = {
             'train': transforms.Compose([
-                transforms.RandomResizedCrop(224),
+                transforms.Resize(224),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 transforms.Normalize([0.485, 0.456, 0.406], [
                                      0.229, 0.224, 0.225])
             ]),
             'val': transforms.Compose([
-                transforms.Resize((256, 256)),
-                transforms.CenterCrop(224),
+                transforms.Resize(224),
                 transforms.ToTensor(),
                 transforms.Normalize([0.485, 0.456, 0.406], [
                                      0.229, 0.224, 0.225])
