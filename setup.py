53da9c93b264e0e1331e889084334bfa6396dae9
nameless-mc <s.taniguchi.zip@gmail.com>
2022-11-17 06:29:08 +0000

    setup の追加

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+def _requires_from_file(filename):
+    with open(filename) as f:
+        ls = f.read().splitlines()
+    return list(filter(lambda x : not (len(x) == 0 or x.startswith('#')), ls))
+
+setup(
+    name = "vgg16-for-glide",
+    install_requires = _requires_from_file('requirements.txt')
+)
